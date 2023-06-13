7cb80861269953b6c607577a996312c366af4285
Marohn Howard <mars.howard@devmounta.in>
2023-06-12 18:23:25 -0600

    real first push

---
+++ marsPageObject.ts
@@ -0,0 +1,7 @@
+import {By} from 'selenium-webdriver'; 
+import {BasePage} from './basePage'
+export class marsPage extends BasePage {
+    //selectors
+    //constuctor
+    //methods
+}
---
+++ marsTest.test.ts
@@ -0,0 +1,3 @@
+import {marsPage} from './marsPageObject'; 
+const mars = new marsPage();
+const fs = require('fs')
